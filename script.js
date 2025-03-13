2a3c6a8b679b27df3e1f8432d8f410431a97ae67
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-03-12 22:13:56 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+
+const form = document.getElementById('form')
+const inputs = document.querySelectorAll('.inputs')
+const spans = document.querySelectorAll('.span-required')
+
+
+document.addEventListener('keydown', (evt) => {
+    if (evt.key == 'Enter') {
+        impedirEnvio(evt)
+    }
+})
+form.addEventListener('submit', impedirEnvio)
+function impedirEnvio(ev) {
+    ev.preventDefault()//Impedir o Envio do Form
+}
+
+
+function CasoError(indice) {
+    inputs[indice].style.border = '2px solid #e63636'
+    spans[indice].style.display = 'block'
+}
+function NotError(indice) {
+    inputs[indice].style.border = ''
+    spans[indice].style.display = 'none'
+}
+
+inputs[0].addEventListener('input')
+inputs[1].addEventListener('input')
+inputs[2].addEventListener('input')
+inputs[3].addEventListener('input')
+inputs[4].addEventListener('input')
+inputs[5].addEventListener('input')
+
+
